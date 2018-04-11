b369d80d5cbec9a430d7ba9fa5d4d82bf01f3e1c
vilnis <vilnis@es.lv>
2018-04-11 16:42:01 +0300

    updates db

--- functions/index.js
+++ functions/index.js
@@ -18,31 +18,30 @@ firebase.initializeApp(config);
 
 var speechdb = firebase.database().ref();
 
-// a. the action name from the make_name Dialogflow intent
-const NAME_ACTION = 'make_name';
-// b. the parameters that are parsed from the make_name intent
-const COLOR_ARGUMENT = 'color';
-const NUMBER_ARGUMENT = 'number';
+const NAME_ACTION = 'scroll';
+const DIRECTION_ARGUMENT = 'direction';
+const PIXELS_ARGUMENT = 'pixels';
 
 
 exports.speech2browser = functions.https.onRequest((request, response) => {
     const app = new App({request, response});
-    console.log('Request headers: ' + JSON.stringify(request.headers));
-    console.log('Request body: ' + JSON.stringify(request.body));
+    // console.log('Request headers: ' + JSON.stringify(request.headers));
+    // console.log('Request body: ' + JSON.stringify(request.body));
 
-
-// c. The function that generates the silly name
     function makeScroll (app) {
+        console.log(NAME_ACTION);
         let direction = app.getArgument(DIRECTION_ARGUMENT);
         let pixels = app.getArgument(PIXELS_ARGUMENT);
-        app.tell('Done');
-        firebase.database().ref().set({
-            action: SCROLL,
+
+        speechdb.set({
+            action: "scroll",
             direction: direction,
             pixels: pixels
         });
+
+        app.tell('Done');
     }
-    // d. build an action map, which maps intent names to functions
+
     let actionMap = new Map();
     actionMap.set(NAME_ACTION, makeScroll);
 
